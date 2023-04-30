dcc7ac41ed1695239aa1dc4a8705fe8a12647599
Daria Musaeva <hipperhipper@mail.ru>
2023-05-01 01:04:04 +0500

    -first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int price = 12345;
+        int rub = 20;
+        int bonus = (price / rub);
+
+        System.out.println( " Миль начислено " + bonus );
+    }
+}
